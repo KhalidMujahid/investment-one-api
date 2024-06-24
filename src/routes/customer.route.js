b1e197d16b02f9cd0d5d4a8f0ba0ddf757b57ffd
KhalidMujahid <66231619+KhalidMujahid@users.noreply.github.com>
2024-06-24 14:42:39 -0700

    Update customer.route.js

--- src/routes/customer.route.js
+++ src/routes/customer.route.js
@@ -1,10 +1,17 @@
 const { Router } = require("express");
 const {
   loginUser,
+  loginwithGoogle,
+  loginGoogle
 } = require("../controllers/customer.controller");
 
 
 const CustomerRouter = Router();
+
+Customer.get("/auth/google",loginwithGoogle);
+
+
+Customer.get("/auth/google/callback",loginGoogle);
 
 // @Dec: Login user
 // @Method: POST
