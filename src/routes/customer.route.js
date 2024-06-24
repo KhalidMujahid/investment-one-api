f883a3c75559100bd50839cee484361f92820fd9
KhalidMujahid <66231619+KhalidMujahid@users.noreply.github.com>
2024-06-23 23:10:07 -0700

    Update customer.route.js

--- src/routes/customer.route.js
+++ src/routes/customer.route.js
@@ -1,16 +1,10 @@
 const { Router } = require("express");
 const {
-  createAccount,
   loginUser,
 } = require("../controllers/customer.controller");
 
 
 const CustomerRouter = Router();
-
-// @Dec: create account
-// @Method: POST
-// @Access: Public
-CustomerRouter.post("/create", createAccount);
 
 // @Dec: Login user
 // @Method: POST
