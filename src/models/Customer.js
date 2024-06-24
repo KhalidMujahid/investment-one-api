5416488c8d33a26b235a9a64e9b1d996077b03c1
KhalidMujahid <66231619+KhalidMujahid@users.noreply.github.com>
2024-06-23 23:07:43 -0700

    Update Customer.js

--- src/models/Customer.js
+++ src/models/Customer.js
@@ -2,25 +2,9 @@ const { Schema, model } = require("mongoose");
 
 const CustomerSchema = new Schema(
   {
-    first_name: {
-      type: String,
-      required: true,
-    },
-    last_name: {
-      type: String,
-      required: true,
-    },
     email: {
       type: String,
       required: true,
-    },
-    password: {
-      type: String,
-      required: true,
-    },
-    phone_number: {
-      type: String,
-      default: ""
     },
     role: {
       type: Number,
