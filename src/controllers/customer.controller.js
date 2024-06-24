46a76372823d6bb4a4a5a9430105201e940e52cc
KhalidMujahid <66231619+KhalidMujahid@users.noreply.github.com>
2024-06-23 22:54:34 -0700

    Update customer.controller.js

--- src/controllers/customer.controller.js
+++ src/controllers/customer.controller.js
@@ -1,40 +1,4 @@
 const Customer = require("../models/Customer");
-//const bcrypt = require("bcrypt");
-
-
-
-// @Dec: Create account
-// @Access: Public
-// @Method: POST
-module.exports.createAccount = async (req, res, next) => {
-  try {
-    const { first_name, last_name,phone_number, email, password } = req.body;
-
-    if (!first_name || !last_name || !email || !password) {
-      return res.status(400).send({ message: "Credentials are required!" });
-    }
-
-    // first check if email does not exit
-    const check = await Customer.findOne({ email });
-    if (check)
-      return res.status(401).send({ message: "Account with this email has already exit" });
-
-    // hashed password
-    //const hashPassword = await bcrypt.hash(password, 13);
-
-    await Customer.create({
-      first_name,
-      last_name,
-      phone_number,
-      email,
-      password,
-    })
-      .then((data) => res.status(201).send({ data }))
-      .catch((error) => res.status(400).send({ error }));
-  } catch (error) {
-    next(error);
-  }
-};
 
 // @Dec: Login
 // @Access: Private
@@ -43,7 +7,6 @@ module.exports.createAccount = async (req, res, next) => {
 module.exports.loginUser = async (req, res, next) => {
   try {
     // validation
-
     const { email, password: pass } = req.body;
 
     if (!email || !pass)
