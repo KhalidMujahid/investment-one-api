17e7932bd005ff09f0e20c15c011e118abc9aa27
KhalidMujahid <66231619+KhalidMujahid@users.noreply.github.com>
2024-06-24 14:29:35 -0700

    Update customer.controller.js

--- src/controllers/customer.controller.js
+++ src/controllers/customer.controller.js
@@ -1,4 +1,46 @@
+const axios = require('axios');
 const Customer = require("../models/Customer");
+
+const CLIENT_ID = 'YOUR_CLIENT_ID';
+const CLIENT_SECRET = 'YOUR_CLIENT_SECRET';
+const REDIRECT_URI = '<https://investment-one-api.onrender.com/auth/google/callback>';
+
+
+// @Dec: google
+// @Access: Private
+// Method: Get
+module.exports.loginwithGoogle = async (req,res,next) => {
+  const url = `https://accounts.google.com/o/oauth2/v2/auth?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=code&scope=profile email`;
+  res.status(200).send(url);
+};
+
+// @Dec: google
+// @Access: Private
+// Method: Get
+module.exports.loginGoogle = async (req, res,next) => {
+  const { code } = req.query;
+
+  try {
+    const { data } = await axios.post('https://oauth2.googleapis.com/token', {
+      client_id: CLIENT_ID,
+      client_secret: CLIENT_SECRET,
+      code,
+      redirect_uri: REDIRECT_URI,
+      grant_type: 'authorization_code',
+    });
+
+    const { access_token, id_token } = data;
+
+    
+    const { data: profile } = await axios.get('https://www.googleapis.com/oauth2/v1/userinfo', {
+      headers: { Authorization: `Bearer ${access_token}` },
+    });
+
+    res.redirect('/');
+  } catch (error) {
+    next(error);
+  }
+}
 
 // @Dec: Login
 // @Access: Private
